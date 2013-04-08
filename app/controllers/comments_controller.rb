da02da653744e1c7006dabd2e1cdd395695b430f
naveenruby <naveenkumar.rails@gamil.com>
2013-04-08 15:58:16 +0530

    change

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,9 +2,7 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find params[:post_id]
     @comment = @post.comments.create params[:comment] 
-    
-
-     respond_to do |format|
+    respond_to do |format|
       format.html { redirect_to @video }
       format.js # create.js.erb
   end
